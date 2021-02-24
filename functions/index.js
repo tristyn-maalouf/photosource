1fb4496736aedeaad62004bcf71c25a6214e83ce
Tristyn Maalouf <tmaalouf@google.com>
2021-02-24 07:41:54 -0800

    Use cloud vision api to add tags to images

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,43 @@
 // Import the Firebase SDK for Google Cloud Functions.
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+// Imports the Google Cloud client library
+const vision = require('@google-cloud/vision');
 
-// Adds a message that welcomes new users into the chat.
-exports.testFunction = functions.storage.object().onFinalize(async (photoObject) => {
-    console.log('function worked!');
-});
+exports.addCloudVisionTags = functions.firestore.document('photos/{imageUrl}')
+  .onUpdate((change, context) => {  
+   const after = change.after.data();
+
+    if(after.imageUrl !== 'https://www.google.com/images/spin-32.gif?a'){
+      console.log('a GCS image url has been created for the image')
+      const db = admin.firestore();
+
+      return db.collection('photos').doc(context.params.imageUrl)  // get imageUrl
+      .get()
+      .then(async doc => {
+        var cloudVisionTags = [];
+
+        // Creates a client
+        const client = new vision.ImageAnnotatorClient();
+
+        // Performs label detection on the image file
+        const [result] = await client.labelDetection(doc.data().imageUrl);
+        const labels = result.labelAnnotations;
+        console.log('Labels:');
+        labels.forEach(label => {
+            console.log(label.description);
+            cloudVisionTags.push(label.description.toLowerCase());
+        });
+        var allTags = [];
+        allTags = cloudVisionTags.concat(doc.data().tags);
+        const data = {
+            autoTags: cloudVisionTags,
+            combinedTags: allTags 
+        };
+        admin.firestore().collection('photos').doc(doc.data().id).update(data);
+   })
+   .then(() => console.log('function complete') )
+   .catch(err => console.log(err) )
+    } 
+  });
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -32,10 +32,10 @@ var DEFAULT_IMAGE_ENCODING = '';
 var userEnteredTags = [];
 var photoRefId = '';
 var imgEncoding = '';
+var emptyTagList = [];
 
 // native javascript md5 algorithm.
 var MD5 = function(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
-
 
 // On load of the web app show all photos
 document.addEventListener("DOMContentLoaded", function(){
@@ -83,7 +83,6 @@ function signOut() {
 
 // Returns the signed-in user's profile pic URL.
 function getProfilePicUrl() {
-  console.log("profile pic url: " + firebase.auth().currentUser.photoURL);
   return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
 }
 
@@ -221,9 +220,7 @@ async function onMediaFileSelected(event) {
          if(querySnapshot.empty){
            saveImageMessage(file);
          } else {
-          querySnapshot.forEach(documentSnapshot => {
-            compareImageElement.src = documentSnapshot.data().imageUrl;
-          });
+          compareImageElement.src = querySnapshot.docs[0].data().imageUrl;
           compareImageElement.removeAttribute('hidden');
           modalMessageElement.removeAttribute('hidden');
           saveImageMessage(file);
@@ -239,7 +236,6 @@ function getUsersPhotos() {
   firebase.firestore().collection('photos').where('email', '==', firebase.auth().currentUser.email).get()
     .then(querySnapshot => {
       querySnapshot.forEach(documentSnapshot => {
-        console.log(documentSnapshot.data());
         showPhoto(documentSnapshot.data().imageUrl);
       });
     });
@@ -261,11 +257,10 @@ async function getAllPhotos() {
 // Triggered when a user enters a search term and clicks "Search" from the top bar.
 function getRelevantPhotos() {
   console.log("getting relevant photos");
-  var searchString = document.querySelector('#search-string').value;
-  firebase.firestore().collection('photos').where('tags', 'array-contains', searchString).get()
+  var searchString = document.querySelector('#search-string').value.toLowerCase();
+  firebase.firestore().collection('photos').where('combinedTags', 'array-contains', searchString).get()
     .then(querySnapshot => {
       querySnapshot.forEach(documentSnapshot => {
-        console.log(documentSnapshot.data());
         showPhoto(documentSnapshot.data().imageUrl);
       });
     });
@@ -291,11 +286,13 @@ function saveImageMessage(file) {
     imageUrl: LOADING_IMAGE_URL,
     imageEncoding: DEFAULT_IMAGE_ENCODING,
     timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    tags: ["new tag", "test2", "test3"]
+    tags: ["new tag", "test2", "test3"],
+    id: '',
+    combinedTags: emptyTagList
   }).then(function(photoRef) {
     photoRefId = photoRef.id;
-    // 2 - Upload the image to Cloud Storage.
     var filePath = firebase.auth().currentUser.uid + '/' + photoRef.id + '/' + file.name;
+    console.log("file path: " + filePath);
     return firebase.storage().ref(filePath).put(file).then(function(fileSnapshot) {
       // 3 - Generate a public URL for the file.
       return fileSnapshot.ref.getDownloadURL().then((url) => {
@@ -304,10 +301,11 @@ function saveImageMessage(file) {
         showPhoto(url);
         // 4 - Update the file url placeholder with the image’s URL.
         return photoRef.update({
+          id: photoRefId,
           imageUrl: url,
           storageUri: fileSnapshot.metadata.fullPath,
           tags: userEnteredTags,
-          imageEncoding: imgEncoding
+          imageEncoding: imgEncoding    
         });
       });
     });
